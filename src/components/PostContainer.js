8ac36668f66099a21e8b9a2d4aaceed1948c5654
Alexis S Liu <syl1350@gmail.com>
2021-08-23 17:20:03 -0400

    semantic ui setup

--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -3,9 +3,21 @@ import PostCard from "./PostCard";
 function PostContainer() {
   return (
     <>
-      <div className="ui segment">
-          <p>this is PostContainer</p>
-          <PostCard />
+      <div className="ui main container">
+        <div className="ui grid">
+          <div className="ten wide column">
+            <h2 className="ui header">Posts</h2>
+            <PostCard />
+            <p> i am a fake post</p>
+            <p> i am a fake post</p>
+            <p> i am a fake post</p>
+            <p> i am a fake post</p>
+            <p> i am a fake post</p>
+            <p> i am a fake post</p>
+            <p> i am a fake post</p>
+            <p> i am a fake post</p>
+          </div>
+        </div>
       </div>
     </>
   );
