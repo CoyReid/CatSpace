02bd96e727998092a207e1f98ed72502f67eac3c
Alexis S Liu <syl1350@gmail.com>
2021-08-23 19:05:09 -0400

    created sidemenu navlink

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
 import { NavLink } from "react-router-dom";
+import { Menu } from 'semantic-ui-react'
 
 function NavBar() {
     return (
         <div>
-            <NavLink to="/">Main Page</NavLink>
-            <NavLink to="/profile">Profile</NavLink>
-            <NavLink to="/friends">Friends</NavLink>
-            <NavLink to="/settings">Settings</NavLink>
+          <Menu vertical>
+            <Menu.Item as={ NavLink} to="/">Main Page.</Menu.Item>
+            <Menu.Item as={NavLink} to="/profile">Profile</Menu.Item>
+            <Menu.Item as={NavLink} to="Friends">Friends</Menu.Item>
+            <Menu.Item as={NavLink} to="/settings">Settings</Menu.Item>
+         </Menu>
         </div>
     )
 }
