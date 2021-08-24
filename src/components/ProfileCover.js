91ec39a6ed55b3935f07d34a45003bb65e6c84f1
Alexis S Liu <syl1350@gmail.com>
2021-08-24 18:45:54 -0400

    ProfileCover

--- src/components/ProfileCover.js
+++ src/components/ProfileCover.js
@@ -4,15 +4,16 @@ export default function ProfileCover(){
 
   return (
 
-<Card fluid color='red'>
+<Segment className="profileCover" raised>
 
-<img class="ui fluid image" src='https://live.staticflickr.com/4250/34389735790_9045d855bc_o.jpg' size='large' bordered />
+<img className="ui fluid image" src='https://live.staticflickr.com/4250/34389735790_9045d855bc_o.jpg' size='large'/>
  <Container>
 
-   <img class="ui small circular image" style= {{position:"relative", left:"5px", top:"-60px"}} src="https://i.ibb.co/fnTcqdc/600-Grumpy.jpg"/>
-   <h3> username </h3>
+   <img class="ui small circular image" style= {{position:"relative", left:"5px", top:"-80px", }} src="https://i.ibb.co/fnTcqdc/600-Grumpy.jpg"/>   
+   <h3 > username </h3>
+
    </Container>
-  </Card>
+   </Segment>
 
   )
 }
