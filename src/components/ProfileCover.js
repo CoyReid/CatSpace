4e590fad20c34506703b1673ba1c1bf5731184ea
Alexis S Liu <syl1350@gmail.com>
2021-08-24 22:47:04 -0400

    ProfileCover component css

--- src/components/ProfileCover.js
+++ src/components/ProfileCover.js
@@ -1,19 +1,27 @@
-
-import { Container, Segment, Image, Card} from "semantic-ui-react";
-export default function ProfileCover(){
-
+import { Container, Segment, Image, Item } from "semantic-ui-react";
+export default function ProfileCover() {
   return (
+    <Segment className="profileCover" raised>
+      <img
+        className="ui fluid image"
+        src="https://live.staticflickr.com/4250/34389735790_9045d855bc_o.jpg"
+        size="large"
+      />
 
-<Segment className="profileCover" raised>
-
-<img className="ui fluid image" src='https://live.staticflickr.com/4250/34389735790_9045d855bc_o.jpg' size='large'/>
- <Container>
-
-   <img class="ui small circular image" style= {{position:"relative", left:"5px", top:"-80px", }} src="https://i.ibb.co/fnTcqdc/600-Grumpy.jpg"/>   
-   <h3 > username </h3>
-
-   </Container>
-   </Segment>
-
-  )
+      <Item.Group>
+        <Item>
+          {/* <Item.Image */}
+          <img 
+            className="profilePic"
+            src="https://i.ibb.co/fnTcqdc/600-Grumpy.jpg"
+            size="small"
+            avatar
+          />
+          <Item.Content className="profilePicText">
+            <Item.Header as="a">Username</Item.Header>
+          </Item.Content>
+        </Item>
+      </Item.Group>
+    </Segment>
+  );
 }
