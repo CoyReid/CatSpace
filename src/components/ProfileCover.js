f3682571634236bc9ed02abf9a2e812a8a978e4f
Alexis S Liu <syl1350@gmail.com>
2021-08-25 14:01:37 -0400

    fixed css, latest version

--- src/components/ProfileCover.js
+++ src/components/ProfileCover.js
@@ -4,26 +4,26 @@ export default function ProfileCover({ profileData }) {
 
   return (
     <Segment className="profileCover">
-      <img
-        className="ui fluid image"
-        id = "coverphoto"
-        src={coverpic}
-        size="large"
-        alt="a cat in wide view"
-      />
-      <Item.Group>
-        <Item>
-          <img
-            className="profilePic"
-            src={image}
-            alt="a cat profile in circle view"
-            size="small"
-          />
-          <Item.Content className="profilePicText">
-            <Item.Header>{name}</Item.Header>
-          </Item.Content>
-        </Item>
-      </Item.Group>
-    </Segment>
+    <img
+      className="ui fluid image"
+      id = "coverphoto"
+      src={coverpic}
+      size="large"
+      alt="a cat in wide view"
+    />
+    <Item.Group>
+      <Item>
+        <img
+          className="profilePic"
+          src={image}
+          alt="a cat profile in circle view"
+          size="small"
+        />
+        <Item.Content className="profilePicText">
+          <Item.Header>{name}</Item.Header>
+        </Item.Content>
+      </Item>
+    </Item.Group>
+  </Segment>
   );
 }
