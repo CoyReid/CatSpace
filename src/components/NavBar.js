e3b6c612aae827bd83594e46c28ed2ed4d302bf3
Alexis S Liu <syl1350@gmail.com>
2021-08-24 23:46:40 -0400

    css logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,12 +4,27 @@ import { Menu } from "semantic-ui-react";
 function NavBar() {
   return (
     <Menu vertical size="large" className="sideMenu">
-      <h2 className="welcome">Welcome to CatSpace</h2>
-      <img className="logo" src="https://www.pngitem.com/pimgs/m/15-152712_cat-kitten-clip-art-cat-icon-transparent-background.png" alt="a cat icon"/>
-      <Menu.Item as={NavLink} to="/">Main Page</Menu.Item>
-      <Menu.Item as={NavLink} to="/profile">Profile</Menu.Item>
-      <Menu.Item as={NavLink} to="/friends">Friends</Menu.Item>
-      <Menu.Item as={NavLink} to="/settings">Settings</Menu.Item>
+      <div className="logoDiv">
+        <img
+          className="logo"
+          src="https://i.ibb.co/zxn66LQ/catlogopurp.png"
+          alt="a cat icon"
+        />
+        <h3 className="welcome">Welcome to CatSpace</h3>
+      </div>
+
+      <Menu.Item as={NavLink} to="/">
+        Main Page
+      </Menu.Item>
+      <Menu.Item as={NavLink} to="/profile">
+        Profile
+      </Menu.Item>
+      <Menu.Item as={NavLink} to="/friends">
+        Friends
+      </Menu.Item>
+      <Menu.Item as={NavLink} to="/settings">
+        Settings
+      </Menu.Item>
     </Menu>
   );
 }
